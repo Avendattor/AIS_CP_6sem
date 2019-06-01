0189dea76430b21088a5188afe4a874ec0053bcc
PaulP <47778996+Avendattor@users.noreply.github.com>
2019-06-01 20:00:03 +0300

    VS + test_3 (Open_Order)

--- VS/AIS_CW/AIS_CW/Program.cs
+++ VS/AIS_CW/AIS_CW/Program.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace AIS_CW
 {
-    class Program_Main
+    class Autotest_Main
     {
         IWebDriver Browser;
         static void Main(string[] args)
@@ -29,6 +29,8 @@ namespace AIS_CW
             IWebElement InputPassword = Browser.FindElement( By.Id("password") );
             //filling field and sending password
             InputPassword.SendKeys( PasswordText + OpenQA.Selenium.Keys.Enter );
+            //waiting 5 seconds 
+            System.Threading.Thread.Sleep(5000);
             //quitting browser after test
             Browser.Quit();
         }
@@ -54,6 +56,35 @@ namespace AIS_CW
             IWebElement LinkContacts = Browser.FindElement( By.PartialLinkText("Контакты") ) ;
             //Clicking on element with Contacts link
             LinkContacts.Click();
+            //waiting 5 seconds 
+            System.Threading.Thread.Sleep(5000);
+            //quitting browser after test
+            Browser.Quit();
+        }
+
+        [Test]
+        public void test_3_Open_Order()
+        {
+            //declaring variable Browser
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            //Maximizing browser window
+            Browser.Manage().Window.Maximize();
+            //opening target site
+            Browser.Navigate().GoToUrl("https://s22851.nubex.ru/");
+            //finding password on page
+            IWebElement Password = Browser.FindElement(By.XPath("/ html / body / div / div[3] / p[2] / strong"));
+            //ejecting password from element
+            String PasswordText = Password.Text;
+            //Searching field to insert password
+            IWebElement InputPassword = Browser.FindElement(By.Id("password"));
+            //filling field and sending password
+            InputPassword.SendKeys(PasswordText + OpenQA.Selenium.Keys.Enter);
+            //Finding element with Order link
+            IWebElement LinkOrder = Browser.FindElement(By.PartialLinkText("Заказать зал"));
+            //Clicking on element with Order link
+            LinkOrder.Click();
+            //waiting 5 seconds 
+            System.Threading.Thread.Sleep(5000);
             //quitting browser after test
             Browser.Quit();
         }
