53abf6cd14d3d7fd4524d7e694b7ac65cf584dcb
PaulP <47778996+Avendattor@users.noreply.github.com>
2019-06-01 12:46:23 +0300

    started VS work

---
+++ VS/AIS_CW/AIS_CW/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+
+/*
+АШТИЭМЭЛЬ.ФАЙЛ.СОЗДАТЬ
+
+ЭТО АШТИЭМЭЛЬ
+        <head>
+            Hello World!
+        </head>
+*/
